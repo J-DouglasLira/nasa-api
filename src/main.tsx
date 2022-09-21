4d867111fa072489489a496c7beac37f69c8e744
J-DouglasLira <jdouglas.developer@gmail.com>
2022-09-21 12:48:44 -0300

    feat: theme provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ThemeProvider } from 'styled-components'
 import './index.css'
 import AuthorizedRoutes from './routes'
+import { theme } from './styles/theme'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <ThemeProvider theme={theme}>
     <AuthorizedRoutes />
+    </ThemeProvider>
   </React.StrictMode>
 )
