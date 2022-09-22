1d50f100b3db925ec0723b1466d6291bde9cd197
J-DouglasLira <jdouglas.developer@gmail.com>
2022-09-22 11:23:14 -0300

    feat: queryClientProvider

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,13 +4,19 @@ import Home from '../pages/home';
 import { ThemeProvider } from 'styled-components';
 import { theme } from '../styles/theme';
 import Details from '../pages/details';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from '../services/queryClient';
+import {ReactQueryDevtools} from '@tanstack/react-query-devtools';
+
 
 
 
 const AuthorizedRoutes: React.FC = () => {
   const history = createBrowserHistory({ basename: '/home' });
   return (
+    
     <ThemeProvider theme={theme}>
+      <QueryClientProvider client={queryClient}>
       <Router history={history}>
       <Switch>
         <Route exact path='/'>
@@ -21,6 +27,8 @@ const AuthorizedRoutes: React.FC = () => {
         </Route>
       </Switch>
     </Router>
+    <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
     </ThemeProvider>
   )
 }
