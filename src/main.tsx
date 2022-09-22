a561faaf0087cb48743ce829443597b28e945775
J-DouglasLira <jdouglas.developer@gmail.com>
2022-09-22 11:23:46 -0300

    style: whitespace

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,9 @@ import ReactDOM from 'react-dom/client'
 import AuthorizedRoutes from './routes';
 import GlobalStyle from './styles/GlobalStyle';
 
-
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <GlobalStyle />
-    <AuthorizedRoutes />
+   <AuthorizedRoutes />
   </React.StrictMode>
 )
