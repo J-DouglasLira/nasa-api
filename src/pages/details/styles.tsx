d9b8b703b90f6c21b0bf905fc4395228bc01d109
J-DouglasLira <jdouglas.developer@gmail.com>
2022-09-29 15:54:57 -0300

    refactor: styled-components

--- src/pages/details/styles.tsx
+++ src/pages/details/styles.tsx
@@ -1,13 +1,5 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
-  background: #312e81;
-`;
-export const Header = styled.div`
-  border-bottom: 1px solid ${({ theme }) => theme.colors.primary.three + '80'};
-  height: 75px;
-`;
-
 export const ImageBackground = styled.img`
   position: absolute;
   top: 0;
@@ -21,10 +13,4 @@ export const ImageBackground = styled.img`
 export const Content = styled.div`
   background: #312e81;
   height: calc(100vh - 80px);
-`;
-
-export const Information = styled.div`
-  display: flex;
-  justify-content: space-around;
-  border-bottom: 1px solid ${({ theme }) => theme.colors.primary.three + '80'};
 `;
