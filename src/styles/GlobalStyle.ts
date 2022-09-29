048342bb1764102514dd60dd36cefa7f5c2493c7
J-DouglasLira <jdouglas.developer@gmail.com>
2022-09-29 15:57:16 -0300

    refactor: styled-components

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -7,12 +7,6 @@ const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
     font-family: 'Inter', sans-serif;
   }
-  /* body{
-    height: 100%;
-    background-image: linear-gradient(to top, #335fee, #4b5fe8, #5b5fe2, #685fdd, #725fd7);
-    background-repeat: no-repeat;
-    background-size: cover;
-  } */
 `;
 
 export default GlobalStyle;
